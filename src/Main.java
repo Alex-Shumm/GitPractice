56694dcd8fb2a328298dae6d5f78b19f236453c3
Alex-Shumm <shumakov1231@gmail.com>
2025-04-17 21:59:14 -0400

    removed prev logic with loops

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Text from Branch 2");
-        System.out.println("First branch text");
-        System.out.println("Hello");
+        System.out.println("Hello guys");
+        System.out.println("Hello guys");
+        System.out.println("Hello guys");
+        System.out.println("Hello guys");
+        System.out.println("Hello guys");
+        System.out.println("Hello guys");
     }
 }
