48af890ad0d474f820d412f3141c61b4eb747d0c
Alex-Shumm <shumakov1231@gmail.com>
2025-04-17 22:03:54 -0400

    One line only

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Text from Branch 2");
-        System.out.println("First branch text");
-        System.out.println("Hello");
+        System.out.println("ddddddddd");
     }
 }
